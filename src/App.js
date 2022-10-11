42793d8c565fd44bed43ae502e8d1f84b6c3ded7
Muhammad Zamroni <51905696+zammy86@users.noreply.github.com>
2022-10-11 18:49:38 +0700

    reducer, iauction, route, layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Suspense } from 'react'
+import Routerq from './router/Routerq';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Routerq />
+    </Suspense>
   );
 }
 
